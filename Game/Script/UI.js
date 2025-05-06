9e2c87c2a6dadbf89755051bff2fc9525c1869aa
pupungeggang2 <kyongkyong2892@gmail.com>
2025-05-06 21:46:24 +0900

    Changed Resolution

--- Game/Script/UI.js
+++ Game/Script/UI.js
@@ -23,6 +23,11 @@ const UI = {
     },
 
     menu: {
-        
-    },
+        rect: [320, 240, 640, 240],
+        textPaused: [328, 324],
+        buttonResume: [320, 320, 640, 80],
+        textResume: [328, 344],
+        buttonExit: [320, 400, 640, 80],
+        textExit: [328, 424],
+    }
 }
