1ba0f522efac1390220a6747778117421d43ebb0
pupungeggang6 <pupungeggang6@gmail.com>
2025-11-30 01:47:30 +0900

    Start

---
+++ src/script/main.js
@@ -0,0 +1,17 @@
+window.onload = main
+window.onerror = errorHandle
+window.oncontextmenu = rightClick
+
+let game
+
+function main() {
+
+}
+
+function errorHandle() {
+
+}
+
+function rightClick() {
+    return false
+}
